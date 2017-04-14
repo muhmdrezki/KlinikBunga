a9db148678b0125c6e8377ec72eece41a8b2741d
Muhamad Rezki <muhmdrezki@gmail.com>
2017-04-14 15:59:06 +0700

    +Bisa select image dari galley HP
    - udah tambah code buat proses upload gambar ke storage firebase
    - masih Belum bisa save ke firebase
    - Image yang diselect masih ga tampil
    - progress bar/dialog ga ilang2, gambar ga keupload.

--- app/src/main/java/com/example/rezki/klinikbunga/PostActivity.java
+++ app/src/main/java/com/example/rezki/klinikbunga/PostActivity.java
@@ -1,35 +1,93 @@
 package com.example.rezki.klinikbunga;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
-import com.google.android.gms.common.images.ImageManager;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.w3c.dom.Text;
 
 public class PostActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText posttitle;
+    private EditText postdesc;
+
+    private Button buttonsubmit;
     private ImageButton imageselect;
+
+    private Uri imageURI = null;
+
+
     private static final int GALLERY_REQUEST = 1;
+    private StorageReference storage;
+    private ProgressDialog progressdialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        imageselect = (ImageButton) findViewById(R.id.imageselect);
+        //Tampung ID di Variable
+        storage = FirebaseStorage.getInstance().getReference();
+        progressdialog = new ProgressDialog(this);
+
+        imageselect     = (ImageButton) findViewById(R.id.imageselect);
+        posttitle       = (EditText) findViewById(R.id.posttitle);
+        postdesc        = (EditText) findViewById(R.id.postdesc);
+        buttonsubmit    = (Button) findViewById(R.id.buttonsubmit);
+
+        //Supaya Tombol/Link Berfungsi
         imageselect.setOnClickListener(this);
+        buttonsubmit.setOnClickListener(this);
     }
 
 
+    //Fungsi Posting
+    private void StartPosting() {
+        //Deklarasi Variable
+        String judul = posttitle.getText().toString().trim();
+        String deskripsi = postdesc.getText().toString().trim();
+
+        //Menampilkan Progress Bar
+        progressdialog.setMessage("Posting, Please Wait");
+        progressdialog.show();
+
+        //Title, Deskripsi, Gambar gabisa kosong
+        if(!TextUtils.isEmpty(judul) && !TextUtils.isEmpty(deskripsi) && imageURI != null){
+        //Proses Upload
+            StorageReference filepath = storage.child("Blog_Images").child(imageURI.getLastPathSegment());
+
+            filepath.putFile(imageURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    progressdialog.dismiss();
+                }
+            });
+        }
+    }
+
     @Override
     public void onClick(View view) {
-        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        galleryIntent.setType("image/*");
-        startActivityForResult(galleryIntent, GALLERY_REQUEST);
+          if(imageselect==view)
+        {
+            Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            galleryIntent.setType("image/*");
+            startActivityForResult(galleryIntent, GALLERY_REQUEST);
+        } else if(buttonsubmit==view){
+            StartPosting();
+        }
     }
 
     @Override
